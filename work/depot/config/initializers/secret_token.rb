1287a99442d7f7f138396d0fe6ea1edec81268c1
Nicholai de Guzman <nicholai@cmu.edu>
2012-02-14 23:50:04 -0500

    initial creation and migration of depot

---
+++ work/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'a31fdfebb4a286d73c6feeb2f7e5613b3e20bb0f139d9a25c9b1e31428f667296d880aa1e2af895338824863c6fb4819c1ea0a19cdeb3eb079e3a9575dc15ca0'
