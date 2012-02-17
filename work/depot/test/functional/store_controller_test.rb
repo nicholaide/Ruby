b119c68436308484ac1c42a18da5aa78b39c3e14
Nicholai de Guzman <nicholai@cmu.edu>
2012-02-17 10:12:11 -0500

    before task D

--- work/depot/test/functional/store_controller_test.rb
+++ work/depot/test/functional/store_controller_test.rb
@@ -4,6 +4,15 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    
+    # we want to check that response contains our layout, prod info and price formatting
+    # based on fixtures: test/fixtures/products.yml (which I modified, but there are still three elements)
+    
+    assert_select '#columns #side a', :minimum => 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+
   end
 
 end
