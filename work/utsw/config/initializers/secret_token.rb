efd8ad1e0d76753d960274b8bc082edb60399421
Nicholai de Guzman <nicholai@cmu.edu>
2012-02-23 09:21:30 -0500

    generated project utsw

---
+++ work/utsw/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utsw::Application.config.secret_token = '0e133323c59aa23a295c05ed1983636b686ec8d68d85aad18e914af4e3274445a713e9decc6fa4a0404d28ee8d66ed14505ca0282916b14d88a067c1ef413414'
