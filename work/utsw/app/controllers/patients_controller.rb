a8bd5144602a6821bef93c295290565d33b321c6
Nicholai de Guzman <nicholai@cmu.edu>
2012-02-25 14:54:52 -0500

    added menu

--- work/utsw/app/controllers/patients_controller.rb
+++ work/utsw/app/controllers/patients_controller.rb
@@ -45,7 +45,8 @@ class PatientsController < ApplicationController
 
     respond_to do |format|
       if @patient.save
-        format.html { redirect_to @patient, :notice => 'Patient was successfully created.' }
+        #format.html { redirect_to @patient, :notice => 'Patient was successfully created.' }
+        format.html { redirect_to front_url, :notice => 'Patient was successfully created.' }
         format.json { render :json => @patient, :status => :created, :location => @patient }
       else
         format.html { render :action => "new" }
@@ -61,7 +62,8 @@ class PatientsController < ApplicationController
 
     respond_to do |format|
       if @patient.update_attributes(params[:patient])
-        format.html { redirect_to @patient, :notice => 'Patient was successfully updated.' }
+        #format.html { redirect_to @patient, :notice => 'Patient was successfully updated.' }
+        format.html { redirect_to front_url, :notice => 'Patient was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render :action => "edit" }
--- work/utsw/config/routes.rb
+++ work/utsw/config/routes.rb
@@ -57,7 +57,7 @@ Utsw::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-    root :to => 'front#index', :as => 'store'
+    root :to => 'front#index', :as => 'front'
 
   # See how all your routes lay out with "rake routes"
 
